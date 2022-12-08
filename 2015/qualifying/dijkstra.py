ab61d6373fb37932a1400038da3349728f6e3508
Doug Tomm <dougtomm@Dougs-MBP.hsd1.ca.comcast.net>
2022-12-07 22:41:22 -0800

    found cause of RTE.

--- 2015/qualifying/dijkstra.py
+++ 2015/qualifying/dijkstra.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # https://codingcompetitions.withgoogle.com/codejam/round/0000000000433515/0000000000433a60#problem
-
+import random
 from pprint import pprint
 import fileinput
 import unittest
@@ -120,36 +120,35 @@ def solution(s, x):
 
         result = '1'
         i = 0
-        i_bound = None
+        i_prefix_exists = False
         while i < len(s):
             result = multiply(result, s[i])
-            if result == 'i':
-                i_bound = i
-
             i += 1
-            if i_bound is not None:
+            if result == 'i':
+                i_prefix_exists = True
                 break
 
-        j_bound = None
+        if not i_prefix_exists:
+            return False
+
+        j_prefix_exists = None
         result = '1'
         while i < len(s):
             result = multiply(result, s[i])
-            if result == 'j':
-                j_bound = i
-
             i += 1
-            if j_bound is not None:
+            if result == 'j':
+                j_prefix_exists = True
                 break
 
-        return i_bound is not None and j_bound is not None
+        return j_prefix_exists
 
     if reduction[-1] == '1':
         return False
 
     remainder = x % 4
-    result = reduction
+    result = '1'
     for i in range(remainder):
-        result = multiply(result, s[i])
+        result = multiply(result, reduction)
 
     if result != '-1':
         return False
@@ -159,30 +158,41 @@ def solution(s, x):
 
     result = '1'
     i = 0
-    i_bound = None
-    while i < len(s):
-        result = multiply(result, s[i])
+    i_prefix_exists = False
+
+    # look for 'i' prefix. multiply the chars of s together until we get 'i'.  traverse s up to 4 times.
+    # any string ** 4 is always 1.  if we haven't found an 'i' prefix by the time we've traversed s 4 times
+    # we never will.
+
+    stop_already = 4 * len(s)
+
+    while i < stop_already:
+        idx = i % len(s)
+        result = multiply(result, s[idx])
+        i += 1
         if result == 'i':
-            i_bound = i
-
-        i = (i + 1) % len(s)
-        if i_bound is not None:
+            i_prefix_exists = True
             break
 
-    j_bound = None
-    while i < len(s):
-        result = multiply(result, s[i])
+    if not i_prefix_exists:
+        return False
+
+    j_prefix_exists = None
+    d = i % len(s)
+    i = d
+    result = '1'
+    while i < stop_already + d:
+        idx = i % len(s)
+        result = multiply(result, s[idx])
+        i += 1
         if result == 'j':
-            j_bound = i
-
-        i = (i + 1) % len(s)
-        if j_bound is not None:
+            j_prefix_exists = True
             break
 
-    return i_bound is not None and j_bound is not None
+    return j_prefix_exists
 
 
-if __name__ == "__main__":
+def showtime():
     fi = fileinput.FileInput()
 
     # throw away first line
@@ -196,14 +206,55 @@ if __name__ == "__main__":
         x = int(x)
         line = fi.readline().strip()
         s = line
-        #print("s = %s, x = %s" % (s, x))
+        # print("s = %s, x = %s" % (s, x))
         result = solution(s, x)
         print("Case #%s: %s" % (case_number, "YES" if result else "NO"))
         case_number += 1
         line = fi.readline().strip()
 
 
+def stresstest():
+    # generate inputs and run the test until something breaks.
+
+    while True:
+        length = random.randint(1, 500)
+        s = ''.join(random.choices("ijk", k=length))
+        x = random.randint(1, 10000)
+        try:
+
+            #print(s, len(s))
+            #print(x)
+            solution(s, x)
+        except Exception as e:
+            print("length = %s, x = %s" % (length, s))
+            print(e)
+
+
+if __name__ == '__main__':
+    showtime()
+    
+
 class MyTest(unittest.TestCase):
+    def test_5(self):
+        s = 'iii'
+        x = 6
+        self.assertFalse(solution(s, x))
+
+    def test_4(self):
+        s = 'ii'
+        x = 6
+        self.assertFalse(solution(s, x))
+
+    def test_3(self):
+        s = 'i'
+        x = 6
+        self.assertFalse(solution(s, x))
+
+    def test_2_5(self):
+        s = 'ij'
+        x = 2
+        self.assertTrue(solution(s, x))
+
     def test_2(self):
         s = 'ji'
         x = 6
@@ -213,4 +264,3 @@ class MyTest(unittest.TestCase):
         s = 'ijk'
         x = 1
         self.assertTrue(solution(s, x))
-
