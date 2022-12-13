fe0e19a239322a7ed53cf7196636b4ea49b21eab
Doug Tomm <dougtomm@Dougs-MBP.hsd1.ca.comcast.net>
2022-12-13 15:02:29 -0800

    accepted

--- 2009/qualifying/language.py
+++ 2009/qualifying/language.py
@@ -5,44 +5,45 @@
 from pprint import pprint
 import fileinput
 import unittest
-import re
 
+def split_pattern(pattern):
+    # return the pattern as a list of disjunctions or letters.
+    # ex:  a(bcd)ef(gh)i()j ==> a bcd e f gh i j
 
-def pattern_length(pattern):
-    # how many characters will a pattern match?
-    length = 0
+    pieces = []
     in_parens = False
-    paren_at = None
+
     i = 0
+    paren_at = None
     for c in pattern:
         if c == '(':
-            paren_at = i
             in_parens = True
+            paren_at = i
         elif c == ')':
             in_parens = False
-            nchoices = i - paren_at - 1
-            if nchoices > 0:
-                length += 1
+            piece = pattern[paren_at + 1:i]
+            if len(piece) > 0:
+                pieces.append(piece)
         elif not in_parens:
-            length += 1
+            pieces.append(c)
         i += 1
 
-    return length
+    return pieces
 
 
 def solution(language, pattern):
     wordlength = len(language[0])
-    if pattern_length(pattern) != wordlength:
+    pieces = split_pattern(pattern)
+    if wordlength != len(pieces):
         return 0
-    
-    pattern = pattern.replace('(', '[').replace(')', ']')
-    #pprint(pattern)
-    result = 0
-    for word in language:
-        m = re.match(pattern, word)
-        if bool(m):
-            result += 1
-    return result
+
+    filtered_language = language
+    for i in range(wordlength):
+        filtered_language = list(filter(lambda x: x[i] in pieces[i], filtered_language))
+        if len(filtered_language) == 0:
+            break
+
+    return len(filtered_language)
 
 
 if __name__ == "__main__":
@@ -63,8 +64,8 @@ if __name__ == "__main__":
         pattern = fi.readline().strip()
         patterns.append(pattern)
 
-    #pprint(language)
-    #pprint(patterns)
+    # pprint(language)
+    # pprint(patterns)
 
     case_number = 1
     for p in patterns:
@@ -72,7 +73,7 @@ if __name__ == "__main__":
         print("Case #%s: %s" % (case_number, result))
         case_number += 1
 
-              
+
 class MyTest(unittest.TestCase):
     def test_1(self):
         language = ['abc', 'bca', 'dac', 'dbc', 'cba']
@@ -80,11 +81,12 @@ class MyTest(unittest.TestCase):
         expecting = 2
         self.assertEqual(expecting, solution(language, pattern))
 
-    def test_pattern_length(self):
-        self.assertEqual(0, pattern_length(""))
-        self.assertEqual(1, pattern_length("a"))
-        self.assertEqual(3, pattern_length("a(d)t"))
-        self.assertEqual(1, pattern_length("(d)"))
-        self.assertEqual(0, pattern_length("()"))
-        self.assertEqual(3, pattern_length("ae()t"))
+    def test_2(self):
+        language = ['abc', 'bca', 'dac', 'dbc', 'cba']
+        pattern = '(zyx)bc'
+        expecting = 0
+        self.assertEqual(expecting, solution(language, pattern))
 
+    def test_split_pattern(self):
+        pattern = "a(bcd)ef(gh)i()j"
+        pprint(split_pattern(pattern))
